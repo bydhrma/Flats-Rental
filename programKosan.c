66e899a38717b74c01c59082a14e923ac127bd06
jeaconia <tjeaconia8@gmail.com>
2023-12-10 22:41:45 +0800

    kerangka hehe

---
+++ programKosan.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+    int type;
+
+    printf("=========================================\n");
+    printf("|              SERNIA KOST              |\n");
+    printf("=========================================\n");
+    printf("|  TIPE KAMAR                           |\n");
+    printf("|  1. Standard                          |\n");
+    printf("|  2. Deluxe                            |\n");
+    printf("|  3. Grand Deluxe                      |\n");
+    printf("-----------------------------------------\n");
+    printf("|  Pilihan Tipe Kamar:                  |");
+    scanf("%d", &type);
+}
